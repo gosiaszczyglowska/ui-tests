dd7a94aa17fc6067505a2465bbc749ebe38e7ebc
Malgorzata Szczyglowska <Malgorzata_Szczyglowska@epam.com>
2024-07-01 11:54:06 +0200

    Adding some corrections

--- Epam/Class1.cs
+++ Epam/Class1.cs
@@ -1,11 +1,11 @@
 ﻿using System.Linq;
 using System;
-using System.Reflection.PortableExecutable;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System.Threading;
 
 
@@ -19,7 +19,6 @@ namespace EpamTests
         [SetUp]
         public void SetUp()
         {
-
             driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Manage().Window.Maximize();
@@ -31,8 +30,8 @@ namespace EpamTests
             driver.Quit();
         }
 
-
         [TestCase("Java", "All Locations")]
+        [TestCase("Python", "All Cities in Poland")]
         public void PositionSearch(string language, string location)
         {
             driver.Navigate().GoToUrl(@"https://www.epam.com/");
@@ -58,8 +57,16 @@ namespace EpamTests
             keyword.Click();
             keyword.SendKeys(language);
 
-            ///click on the dropdown input, clicking left control to move the dropdown up
-            IWebElement dropdown = searchPanel.FindElement(By.ClassName("select2-selection__rendered"));
+            // Locate the autocomplete dropdown element and close it
+            IWebElement autocompleteDropdown = driver.FindElement(By.ClassName("autocomplete-suggestions"));
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript("arguments[0].style.display='none';", autocompleteDropdown);
+
+            //wait for some time to see the result
+            Thread.Sleep(3000);
+
+            ///click on the dropdown input, click left control to move the dropdown up
+            IWebElement dropdown = searchPanel.FindElement(By.CssSelector("span.select2-selection__rendered[title='All Cities in Poland']"));
             dropdown.Click();
 
             new Actions(driver)
@@ -75,20 +82,40 @@ namespace EpamTests
             remoteCheckbox.Click();
 
             //Clicking on "Find" button
-            IWebElement findButton = searchPanel.FindElement(By.XPath("//*[@type = 'submit']"));
+            IWebElement findButton = searchPanel.FindElement(By.XPath("//*[@id = 'jobSearchFilterForm']/button"));
             findButton.Click();
 
-            //Waiting for the page loading
-            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            // Waiting for the page to load
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@class='search-result__sorting-label list' and text()='Date']")));
 
-            //Sorting results by Date
-            IWebElement searchHeader = driver.FindElement(By.ClassName("search-result__header"));
-            IWebElement sortByDate = searchHeader.FindElement(By.XPath("//li[@title='Date']"));
+            // Get the title of the first item before sorting
+            string initialFirstItemTitle = driver.FindElement(By.XPath("//*[@id='main']//ul/li[1]//h5/a")).Text;
+            Console.WriteLine(initialFirstItemTitle);
+            
+            var sortByDate = driver.FindElement(By.XPath("//*[@class='search-result__sorting-label list' and text()='Date']"));
             sortByDate.Click();
 
             //Waiting until sorting is done
             Thread.Sleep(8000);
-            WebDriverWait wait5 = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            WebDriverWait waitUntilSorted = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+
+            //checking if sorting was done - (if 1st position was changed.
+            //if it wasnt changed after 3 seconds, assuming that sorting was done
+            //but the same position is 1st on the list again)
+            bool isSorted = waitUntilSorted.Until(driver =>
+            {
+                string newFirstItemTitle = driver.FindElement(By.XPath("//*[@id='main']//ul/li[1]//h5/a")).Text;
+                Console.WriteLine(newFirstItemTitle);
+                return newFirstItemTitle != initialFirstItemTitle;
+            });
+
+            if (!isSorted)
+            {
+                Console.WriteLine("The list was not sorted within 3 seconds. Proceeding with the initial order.");
+            }
+
+            string newFirstItemTitle = driver.FindElement(By.XPath("//*[@id='main']//ul/li[1]//h5/a")).Text;
+            Console.WriteLine(newFirstItemTitle);
 
             //Clicking on the 1st search result's "View and apply" button
             IWebElement resulItem1 = driver.FindElement(By.XPath("//*[@id='main']//ul/li[1]"));
@@ -98,12 +125,10 @@ namespace EpamTests
             //waiting for the page to load
             WebDriverWait wait3 = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
 
-            //checking if searched language is on the page
+
+          //checking if searched language is on the page
             bool isTextPresent = driver.FindElements(By.XPath($"//*[contains(text(), {language})]")).Count > 0;
             Assert.IsTrue(isTextPresent, $"Text {language} not found on the page.");
-
-            //time to verify that correct page was opened
-            Thread.Sleep(8000);
         }
 
         [TestCase("BLOCKCHAIN")]
@@ -122,6 +147,8 @@ namespace EpamTests
                 return (element != null && element.Displayed && element.Enabled) ? element : null;
             });
             cookies.Click();
+
+            Thread.Sleep(3000);
 
             //Clicking Search icon
             var searchIcon = driver.FindElement(By.ClassName("dark-iconheader-search__search-icon"));
@@ -145,17 +172,22 @@ namespace EpamTests
                 .Perform();
 
             //Clicking on Find button
-            var findButton = searchPanel.FindElement(By.XPath(".//*[@class='search-results__input-holder']/following-sibling::button"));
+            var findButton = searchPanel.FindElement(By.XPath("//button[contains(span/text(), 'Find')]"));
             findButton.Click();
 
             //Waiting for results
             var searchResultsWait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            var searchResults = searchResultsWait.Until(driver => driver.FindElements(By.XPath("//a[contains(@class, 'search-results__item')]")));
 
-            // Validate that all links contain the query word
-            bool allLinksContainQuery = searchResults.All(link => link.Text.Contains(query, StringComparison.OrdinalIgnoreCase));
+            // Assuming search results are displayed as a list of anchor elements (<a>)
+            var searchResultLinks = searchResultsWait.Until(driver =>
+                driver.FindElements(By.ClassName("search-results__title-link")));
 
-            Assert.IsTrue(allLinksContainQuery, $"Not all links contain the word '{query}'.");
-        }
+            // Validating links using LINQ
+            var linksContainQuery = searchResultLinks.All(link =>
+                link.Text.Contains(query) || link.GetAttribute("href").Contains(query));
+
+            // Assertion for NUnit test case
+            Assert.IsTrue(linksContainQuery, $"Not all links contain the query term '{query}'");
+        }                                  
     }
 }
