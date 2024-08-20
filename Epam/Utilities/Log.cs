19bd6d9a72fcbf67f761da1875c63d0a9d836cdc
Malgorzata Szczyglowska <Malgorzata_Szczyglowska@epam.com>
2024-08-20 10:20:56 +0200

    Log class updated

--- Epam/Utilities/Log.cs
+++ Epam/Utilities/Log.cs
@@ -1,12 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using log4net;
+using System;
+using System.Reflection;
 
 namespace PageObject.Utilities
 {
-    internal class Log
+    public static class Log
     {
+        // Static logger instance
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        // Method to log info messages
+        public static void LogInfo(string message)
+        {
+            if (Logger.IsInfoEnabled)
+            {
+                Logger.Info(message);
+            }
+        }
+
+        // Method to log error messages with exception
+        public static void LogError(string message, Exception ex)
+        {
+            if (Logger.IsErrorEnabled)
+            {
+                Logger.Error(message, ex);
+            }
+        }
+
+        // Method to log debug messages
+        public static void LogDebug(string message)
+        {
+            if (Logger.IsDebugEnabled)
+            {
+                Logger.Debug(message);
+            }
+        }
+
+        // Add other logging methods as needed (e.g., Warn, Fatal)
+        public static void LogWarn(string message)
+        {
+            if (Logger.IsWarnEnabled)
+            {
+                Logger.Warn(message);
+            }
+        }
+
+        public static void LogFatal(string message, Exception ex)
+        {
+            if (Logger.IsFatalEnabled)
+            {
+                Logger.Fatal(message, ex);
+            }
+        }
     }
 }
